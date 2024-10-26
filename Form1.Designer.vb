53b8e889c3949b245e5dbe2453c75ceb81a8fe04
Uliseselestudiante <emmanuelgonza534@gmail.com>
2024-10-26 18:36:06 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        ComboBox3 = New ComboBox()
+        Tbx_resultado = New TextBox()
+        Tbx_cantidad = New TextBox()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(159, 229)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(422, 226)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 1
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Location = New Point(328, 117)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(121, 23)
+        ComboBox3.TabIndex = 2
+        ' 
+        ' Tbx_resultado
+        ' 
+        Tbx_resultado.Location = New Point(432, 275)
+        Tbx_resultado.Name = "Tbx_resultado"
+        Tbx_resultado.Size = New Size(100, 23)
+        Tbx_resultado.TabIndex = 4
+        ' 
+        ' Tbx_cantidad
+        ' 
+        Tbx_cantidad.Location = New Point(168, 275)
+        Tbx_cantidad.Name = "Tbx_cantidad"
+        Tbx_cantidad.Size = New Size(100, 23)
+        Tbx_cantidad.TabIndex = 5
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(61, 4)
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(215, 120)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(107, 15)
+        Label1.TabIndex = 6
+        Label1.Text = "Unidad a convertir:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 15F)
+        Label2.Location = New Point(242, 54)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(257, 28)
+        Label2.TabIndex = 7
+        Label2.Text = "Convertidor Ulises Gonzalez"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(46, 232)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(107, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Unidad a convertir "
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(312, 229)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(104, 15)
+        Label4.TabIndex = 9
+        Label4.Text = "Unidad a convertir"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Tbx_cantidad)
+        Controls.Add(Tbx_resultado)
+        Controls.Add(ComboBox3)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Tbx_resultado As TextBox
+    Friend WithEvents Tbx_cantidad As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+
+End Class
